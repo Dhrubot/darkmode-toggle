72c022980b2eccc67c844dc092923c2169818acd
Dhrubot <dhrubo.dh@gmail.com>
2020-12-16 03:05:39 -0500

    Finished the toggle capabilty and added CSSbaseline

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { useState }from 'react'
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import IconButton from "@material-ui/core/IconButton";
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import BrightnessHighIcon from '@material-ui/icons/BrightnessHigh';
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App = () => {
 
   const [theme, setTheme] = useState(true)
-  const currentTheme = createMuiTheme()
-  console.log(currentTheme)
+  const currentTheme = createMuiTheme(theme ? light : dark)
+  const icon = theme ? <Brightness4Icon /> : <BrightnessHighIcon />
+  
   return (
     <ThemeProvider theme={currentTheme}>
-      <h1>Hello World!!!</h1>
+      <CssBaseline />
+      <IconButton onClick={() => setTheme(!theme)}>
+        { icon }
+      </IconButton>
+      <Typography>
+        {theme ? <h2>Lights On</h2> : <h3>Lights Out</h3>}
+      </Typography>
     </ThemeProvider>
   )
 }
